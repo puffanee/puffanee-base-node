3ebf5eff25bb6f722da703ada352fb6b88c24d3e
Puffanee <puffanee@gmail.com>
2024-09-29 00:06:20 +0300

    src data

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 /* !!!!!! DO NOT CHANGE !!!!!! */
-export const version = "3.1.9";
+export const version = "3.1.0";
 export const hash = "713f5a04ffa19e46dc40a4770393449aa71545f91875dd226f7397b842c22d3732736d5eadf006239ef44f275b9059941989dc56cdfe904c548d52bc52ce4639";
-export const test = true;
 /* !!!!!! DO NOT CHANGE !!!!!! */
+
+export { PuffaneeAPI } from "./dist/api/PuffaneeAPI.js";
+export { PuffaneeMediaStorage, PuffaneeMediaCheck } from "./dist/storage/index.js";
+export {
+  PuffaneeTime,
+  PuffaneeCustomDiscord,
+  PuffaneeSupportJS,
+} from "./dist/helper/index.js";
+export { PuffaneeLogs } from "./dist/log/PuffaneeLogs.js";
+export { PuffaneeFilter } from "./dist/filter/PuffaneeFilter.js";
+export { PuffaneeClient } from "./dist/security/PuffaneeClient.js";
+export { PuffaneeWebPanel } from "./dist/panel/PuffaneeWebPanel.js";
+export function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
