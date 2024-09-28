7e577f218a114fd2544e4a97580fc63db57521cd
Puffanee <mrtkymz35@hotmail.com>
2024-09-28 18:07:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+/* !!!!!! DO NOT CHANGE !!!!!! */
+export const version = "3.0.0";
+export const hash = "713f5a04ffa19e46dc40a4770393449aa71545f91875dd226f7397b842c22d3732736d5eadf006239ef44f275b9059941989dc56cdfe904c548d52bc52ce4639";
+/* !!!!!! DO NOT CHANGE !!!!!! */
