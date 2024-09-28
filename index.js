be2b0f32e4e1000be59cdc5fa9f0b6fbd699e34b
Puffanee <puffanee@gmail.com>
2024-09-29 00:10:40 +0300

    version update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* !!!!!! DO NOT CHANGE !!!!!! */
-export const version = "3.1.0";
+export const version = "3.1.1";
 export const hash = "713f5a04ffa19e46dc40a4770393449aa71545f91875dd226f7397b842c22d3732736d5eadf006239ef44f275b9059941989dc56cdfe904c548d52bc52ce4639";
 /* !!!!!! DO NOT CHANGE !!!!!! */
 
