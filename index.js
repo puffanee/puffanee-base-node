11927e0c80cbe3b4c3033165d1a6ae5c5f0a7c06
Ahmet Mert 'Puffanee' Kaymaz <mrtkymz35@hotmail.com>
2024-09-29 14:46:43 +0300

    3.1.6 - Hash Delete

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import { t } from "tasai";
 
 /* !!!!!! DO NOT CHANGE !!!!!! */
-export const version = "3.1.5";
-export const hash =
-  "713f5a04ffa19e46dc40a4770393449aa71545f91875dd226f7397b842c22d3732736d5eadf006239ef44f275b9059941989dc56cdfe904c548d52bc52ce4639";
+export const version = "3.1.6";
 /* !!!!!! DO NOT CHANGE !!!!!! */
 
 export { PuffaneeAPI } from "./dist/api/PuffaneeAPI.js";
